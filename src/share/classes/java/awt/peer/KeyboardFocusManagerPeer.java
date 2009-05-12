b2677b609434920a31733d5a9f3f431a883143d1
peytoia <peytoia@localhost>
2009-05-12 15:21:55 +0900

    6834474: (tz) Support tzdata2009g Reviewed-by: okutsu

---
+++ src/share/classes/java/awt/peer/KeyboardFocusManagerPeer.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2003-2007 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+
+package java.awt.peer;
+
+import java.awt.Component;
+import java.awt.Window;
+
+/**
+ * The native peer interface for {@link KeyboardFocusManager}.
+ */
+public interface KeyboardFocusManagerPeer {
+
+    /**
+     * Returns the currently focused window.
+     *
+     * @return the currently focused window
+     *
+     * @see KeyboardFocusManager#getNativeFocusedWindow()
+     */
+    Window getCurrentFocusedWindow();
+
+    /**
+     * Sets the component that should become the focus owner.
+     *
+     * @param comp the component to become the focus owner
+     *
+     * @see KeyboardFocusManager#setNativeFocusOwner(Component)
+     */
+    void setCurrentFocusOwner(Component comp);
+
+    /**
+     * Returns the component that currently owns the input focus.
+     *
+     * @return the component that currently owns the input focus
+     *
+     * @see KeyboardFocusManager#getNativeFocusOwner()
+     */
+    Component getCurrentFocusOwner();
+
+    /**
+     * Clears the current global focus owner.
+     *
+     * @param activeWindow
+     *
+     * @see KeyboardFocusManager#clearGlobalFocusOwner()
+     */
+    void clearGlobalFocusOwner(Window activeWindow);
+
+}
