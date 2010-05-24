c2b403a46649d9e905cef66a62908793767056ec
lana <lana@localhost>
2010-05-24 09:06:11 -0700

    Merge

---
+++ src/share/classes/javax/swing/plaf/synth/SynthCheckBoxUI.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2002-2003 Sun Microsystems, Inc.  All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ */
+
+package javax.swing.plaf.synth;
+
+import javax.swing.JComponent;
+import java.awt.Graphics;
+import javax.swing.plaf.ComponentUI;
+
+
+/**
+ * Provides the Synth L&F UI delegate for
+ * {@link javax.swing.JCheckBox}.
+ *
+ * @author Jeff Dinkins
+ * @since 1.7
+ */
+public class SynthCheckBoxUI extends SynthRadioButtonUI {
+
+    // ********************************
+    //            Create PLAF
+    // ********************************
+    /**
+     * Creates a new UI object for the given component.
+     *
+     * @param b component to create UI object for
+     * @return the UI object
+     */
+    public static ComponentUI createUI(JComponent b) {
+        return new SynthCheckBoxUI();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    protected String getPropertyPrefix() {
+        return "CheckBox.";
+    }
+
+    @Override
+    void paintBackground(SynthContext context, Graphics g, JComponent c) {
+        context.getPainter().paintCheckBoxBackground(context, g, 0, 0,
+                                                  c.getWidth(), c.getHeight());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public void paintBorder(SynthContext context, Graphics g, int x,
+                            int y, int w, int h) {
+        context.getPainter().paintCheckBoxBorder(context, g, x, y, w, h);
+    }
+}
